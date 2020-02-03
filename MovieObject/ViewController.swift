c9e50355756df8361e5bc94353e3f12e189d434d
zino <minzino@hanmail.net>
2020-02-03 21:11:41 +0900

    last commit

--- MovieObject/ViewController.swift
+++ MovieObject/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var x:Int=0
+    var y:Int=250
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBAction func goUp(_ sender: Any) {
+        y -= 5
+        move()
+    }
+    
+    @IBAction func goLeft(_ sender: Any) {
+        x -= 5
+        move()
+    }
+    
+    @IBAction func goRight(_ sender: Any) {
+        x += 5
+        move()
+    }
+    
+    @IBAction func goDown(_ sender: Any) {
+        y += 5
+        move()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func move(){
+        imgView.frame.origin = CGPoint(x: x, y: y)
+    }
 
 }
 
